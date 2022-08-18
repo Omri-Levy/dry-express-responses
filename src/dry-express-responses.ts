75b2453d9d286dd0510993d0e1275d699c86010d
Omri Levy <omri.levy0197@gmail.com>
2022-08-19 00:27:11 +0300

    fix(github actions / bun): moved to pnpm to avoid error on build due to bun

--- src/dry-express-responses.ts
+++ src/dry-express-responses.ts
@@ -2,6 +2,26 @@ import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { generateResponse } from './generate-response';
 
+declare module 'express' {
+	export interface Response {
+		ok(data: any): void;
+
+		created(data: any): void;
+
+		found(data: any): void;
+
+		badRequest(data: any): void;
+
+		unauthorized(data: any): void;
+
+		forbidden(data: any): void;
+
+		notFound(data: any): void;
+
+		internalServerError(data: any): void;
+	}
+}
+
 export const dryExpressResponses = (
 	req: Request,
 	res: Response,
