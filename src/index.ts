6863ca9668ae11c696fdc4cce81c892112cd793d
Omri Levy <omri.levy0197@gmail.com>
2022-08-19 01:09:18 +0300

    fix(dist): fixed dist being outdated

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { FormattedError, GenerateResponse } from './interfaces';
+import {
+	DryResponse,
+	FormattedError,
+	GenerateResponse,
+} from './interfaces';
 import { dryExpressResponses } from './dry-express-responses';
 import { generateResponse } from './generate-response';
-import { DryResponse } from './types';
 
 export { dryExpressResponses, generateResponse };
 export type { DryResponse, FormattedError, GenerateResponse };
