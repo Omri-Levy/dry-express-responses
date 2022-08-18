90b1dc3a615712c132c58e3f51c14ea7cfabbd9b
Omri Levy <omri.levy0197@gmail.com>
2022-08-19 01:08:09 +0300

    fix(dryresponse type): fixed dist not being up to date

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,3 +1,5 @@
+import { Response } from 'express';
+
 export interface FormattedError {
 	field?: string;
 	message: string;
@@ -7,4 +9,20 @@ export interface GenerateResponse {
 	data?: any;
 	message?: string;
 	errors?: Array<FormattedError> | FormattedError;
+}
+
+export interface DryResponse extends Response {
+	ok(payload: Omit<GenerateResponse, 'errors'>): void;
+
+	created(payload: Omit<GenerateResponse, 'errors'>): void;
+
+	badRequest(payload: GenerateResponse): void;
+
+	unauthorized(payload: GenerateResponse): void;
+
+	forbidden(payload: GenerateResponse): void;
+
+	notFound(payload: GenerateResponse): void;
+
+	internalServerError(payload: GenerateResponse): void;
 }
