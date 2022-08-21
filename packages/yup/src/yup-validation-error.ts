5a78335844fc07d25f344dcd3470efe9a5964cd9
Omri Levy <omri.levy0197@gmail.com>
2022-08-21 03:32:34 +0300

    fix(yup errors): now directly iterating over the errors array and passing the correct message/field

--- packages/yup/src/yup-validation-error.ts
+++ packages/yup/src/yup-validation-error.ts
@@ -6,27 +6,18 @@ export class YupValidationError extends DryError {
 	status = StatusCodes.BAD_REQUEST;
 
 	// eslint-disable-next-line n/handle-callback-err
-	constructor(public error: ValidationError | ValidationError[]) {
+	constructor(public error: ValidationError) {
 		super(`Invalid request parameters.`);
 
 		Object.setPrototypeOf(this, YupValidationError.prototype);
 	}
 
 	serializeErrors() {
-		if (Array.isArray(this.error)) {
-			return this.error.map((err) => {
-				return {
-					message: err.message,
-					field: err.path,
-				};
-			});
-		}
-
-		return [
-			{
-				message: this.error.message,
+		return this.error.errors.map((err) => {
+			return {
+				message: err,
 				field: this.error.path,
-			},
-		];
+			};
+		});
 	}
 }
